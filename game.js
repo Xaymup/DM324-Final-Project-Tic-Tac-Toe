1c52cbd27be131bf7b73ad17fc588a78371a4c62
Mohamed Alaa <m-alaa8@ubuntu.com>
2019-12-24 00:26:59 +0200

    Mouse events, basic drawing

--- game.js
+++ game.js
@@ -13,7 +13,41 @@ function drawGrid () {
     ctx.strokeStyle = '#FFFFFF';
     ctx.lineWidth = 5;
     ctx.strokeRect(0, 0, canvasWidth, canvasHeight);
-
 }
 
+function addPiece (mouse) {
+  var xCordinate;
+  var yCordinate;
+
+  for (var x = 0;x < 3;x++) {
+    for (var y = 0;y < 3;y++) {
+      xCordinate = x * squareLength;
+      yCordinate = y * squareLength;
+
+      if (mouse.x >= xCordinate && mouse.x <= xCordinate + squareLength &&
+          mouse.y >= yCordinate && mouse.y <= yCordinate + squareLength){
+            drawShit(xCordinate, yCordinate);
+          }
+        }
+      }
+    }
+
+function drawShit (xCordinate, yCordinate) {
+    ctx.strokeStyle = '#000000';
+    ctx.fillRect(xCordinate, yCordinate, squareLength, squareLength);
+}
+
+function getCanvasMousePosition (event) {
+  var rect = canvas.getBoundingClientRect();
+
+  return {
+    x: event.clientX - rect.left,
+    y: event.clientY - rect.top
+  }
+}
 drawGrid();
+
+canvas.addEventListener('mouseup', function (event) {
+  var canvasMousePosition = getCanvasMousePosition(event);
+  addPiece(canvasMousePosition);
+});
