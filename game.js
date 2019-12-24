1a16e90f1aae34c7999ccc1517d616fec4959a64
Mohamed Alaa <m-alaa8@ubuntu.com>
2019-12-24 02:28:20 +0200

    Registering on grid

--- game.js
+++ game.js
@@ -9,6 +9,12 @@ const canvasHeight = canvas.height;
 const squareLength = canvasWidth / 3;
 
 var turn = 0;
+var step = 0;
+
+var xPlaces = [];
+var oPlaces = [];
+
+var winningConditions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
 
 function drawGrid () {
     ctx.strokeStyle = '#000000';
@@ -22,7 +28,7 @@ function drawGrid () {
 function addPiece (mouse) {
   var xCordinate;
   var yCordinate;
-
+  step = step + 1;
   for (var x = 0;x < 3;x++) {
     for (var y = 0;y < 3;y++) {
       xCordinate = x * squareLength;
@@ -33,17 +39,29 @@ function addPiece (mouse) {
             drawShit(xCordinate, yCordinate);
           }
         }
-      }
     }
+    checkWin();
+}
+
+function checkWin(){
+  for (let i = 0;i<=8;i++){
+    if ()
+  }
+}
 
 function drawShit (xCordinate, yCordinate) {
+  position = (yCordinate/100 * 3 + xCordinate/100);
+  if (!(xPlaces+oPlaces).includes(position)){
   if (turn == 0){
     ctx.fillText("X", xCordinate + 10, yCordinate + squareLength - 10, squareLength);
     turn = 1;
+    xPlaces.push(position);
   }
   else {
     ctx.fillText("O", xCordinate + 10, yCordinate + squareLength - 10, squareLength);
     turn = 0;
+    oPlaces.push(position);
+  }
   }
 }
 
